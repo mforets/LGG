62b0f318e92a35c39f23d1a105ec6983b6ef2322
mforets <marcelo.forets@imag.fr>
2017-04-06 10:08:18 +0200

    restructure

--- lib/__init__.py
+++
@@ -1,5 +0,0 @@
-__all__ = ["PolyhedronFromHSpaceRep", "PolyhedronToHSpaceRep", "BoxInfty",
-          "polyhedron_linear_map", "random_polygon_2d",
-           "opposite_polyhedron","polyhedron_sup_norm",
-           "supp_fun_polyhedron", "supp_fun_ellipsoid",
-           "chebyshev_center"]
--- src/parse_and_update.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-printf "sage: preparsing lgg.sage ... "
-sage --preparse lgg.sage
-printf "done\n"
-mv lgg.sage.py lgg.py
